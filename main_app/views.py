b2300ed3ad5b5d38f1dc073f7e9908597b62c40b
avilla13 <xetox13@gmail.com>
2023-07-25 22:38:35 -0500

    Add cars model to views, adjust templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+cars = [
+   {'make': 'Ford', 'model': 'Mustang', 'year': '1967','engine': 'V8' },
+   {'make': 'Dodge', 'model': 'Charger', 'year': '1969','engine': 'V8' },
+   {'make': 'Dodge', 'model': 'Challenger', 'year': '1970','engine': 'V8' },
+   {'make': 'Chevrolet', 'model': 'Camaro', 'year': '1969','engine': 'V8' }
+   {'make': 'Buick', 'model': 'Grand National', 'year': '1987','engine': 'V6' }
+]
+
 # Define the home view
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
@@ -7,4 +15,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cars_index(request):
+   return render(request, 'cars/index.html', {
+      'cars': cars
+   })
 
